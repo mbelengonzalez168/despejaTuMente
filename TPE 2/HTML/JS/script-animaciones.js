905c879b2728e59136da4928d53a7ab02d522be2
Pierinadigio <pierinadigiorgio@gmail.com>
2023-09-24 14:07:30 -0300

    se agregan popup con js

---
+++ TPE 2/HTML/JS/script-animaciones.js
@@ -0,0 +1,22 @@
+
+document.getElementById('icono-compartir').addEventListener('click', function () {
+    document.getElementById('popup-compartir').style.display = 'block';
+    document.body.classList.add("popup-open");
+  });
+  
+  
+  document.getElementById('btn-cerrar-popup-compartir').addEventListener('click', function () {
+    document.getElementById('popup-compartir').style.display = 'none';
+    document.body.classList.remove("popup-open");
+  });
+
+  document.getElementById('usuario').addEventListener('click', function () {
+    document.getElementById('popup-login').style.display = 'block';
+    document.body.classList.add("popup-open");
+  });
+  
+  
+  document.getElementById('btn-cerrar-popup-login').addEventListener('click', function () {
+    document.getElementById('popup-login').style.display = 'none';
+    document.body.classList.remove("popup-open");
+  });
