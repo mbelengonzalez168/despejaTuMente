3c968fdea07d84281b9475bdeaeb8330105f8c4c
Pierinadigio <pierinadigiorgio@gmail.com>
2023-09-24 14:21:00 -0300

    se agrega popup compras

--- TPE 2/HTML/JS/script-animaciones.js
+++ TPE 2/HTML/JS/script-animaciones.js
@@ -20,3 +20,14 @@ document.getElementById('icono-compartir').addEventListener('click', function ()
     document.getElementById('popup-login').style.display = 'none';
     document.body.classList.remove("popup-open");
   });
+
+  document.getElementById('pop-bolsa').addEventListener('click', function () {
+    document.getElementById('popup-compras').style.display = 'block';
+    document.body.classList.add("popup-open");
+  });
+  
+  
+  document.getElementById('btn-cerrar-popup-compras').addEventListener('click', function () {
+    document.getElementById('popup-compras').style.display = 'none';
+    document.body.classList.remove("popup-open");
+  });
