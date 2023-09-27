c219eebde71c52d97dc14845ee713ccd660dec9e
Pierinadigio <pierinadigiorgio@gmail.com>
2023-09-26 21:18:33 -0300

    se agrega formularios a pagina inicio-registracion

--- TPE 2/HTML/JS/script-animaciones.js
+++ TPE 2/HTML/JS/script-animaciones.js
@@ -31,3 +31,14 @@ document.getElementById('icono-compartir').addEventListener('click', function ()
     document.getElementById('popup-compras').style.display = 'none';
     document.body.classList.remove("popup-open");
   });
+
+  document.getElementById('inicio').addEventListener('click', function () {
+    document.getElementById('form-inicio').style.display = 'block';
+    document.body.classList.add("popup-open");
+  });
+  
+  
+  document.getElementById('registrarse').addEventListener('click', function () {
+    document.getElementById('form-registrarse').style.display = 'block';
+    document.body.classList.remove("popup-open");
+  });
