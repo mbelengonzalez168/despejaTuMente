356706a2529cb2d3e297a2f38f6ae8be6e97b5ca
mbelengonzalez168 <margonzalez@alumnos.exa.unicen.edu.ar>
2023-09-30 23:43:18 -0300

    Se mejora el css

--- TPE 2/HTML/JS/script-animaciones.js
+++ TPE 2/HTML/JS/script-animaciones.js
@@ -1,3 +1,31 @@
+"use stric"
+
+const imagenes = document.querySelectorAll(".imagen-pasos");
+
+document.getElementById('paso1').addEventListener('click', function() {
+    mostrarImagen(0);
+});
+document.getElementById('paso2').addEventListener('click', function() {
+    mostrarImagen(1);
+});
+document.getElementById('paso3').addEventListener('click', function() {
+    mostrarImagen(2);
+});
+document.getElementById('paso4').addEventListener('click', function() {
+    mostrarImagen(3);
+});
+
+mostrarImagen(0);
+
+function mostrarImagen(pos) {
+  for (let i=0; i<imagenes.length; i++) {
+    imagenes[i].classList.remove("visible");
+    imagenes[i].classList.add("oculta");
+  }
+  imagenes[pos].classList.remove("oculta");
+  imagenes[pos].classList.add("visible");
+}
+
 
 document.getElementById('icono-compartir').addEventListener('click', function () {
     document.getElementById('popup-compartir').style.display = 'block';
@@ -43,20 +71,8 @@ document.getElementById('icono-compartir').addEventListener('click', function ()
     document.body.classList.remove("popup-open");
   });
 
-  imagenes = document.querySelectorAll(".imagen-pasos");
-  document.getElementById('paso1').addEventListener('click', mostrarImagen(1));
-  document.getElementById('paso2').addEventListener('click', mostrarImagen(2));
-  document.getElementById('paso3').addEventListener('click', mostrarImagen(3));
-  document.getElementById('paso4').addEventListener('click', mostrarImagen(4));
+
   
-  function mostrarImagen(index) {
-      for (let i = 0; i < imagenes.length; i++) {
-        imagenes[i].style.display = 'none';
-      }
-      imagenes[index - 1].style.display = 'block';
-  }
-  
-  mostrarImagen(1);
 
   
 
