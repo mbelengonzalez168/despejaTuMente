90feb7baacc7ad78154b9c8fb34ac98cfbb39d89
Pierinadigio <pierinadigiorgio@gmail.com>
2023-10-02 15:35:57 -0300

    se agrega efecto botones

--- TPE 2/HTML/JS/js.home.js
+++ TPE 2/HTML/JS/js.home.js
@@ -1,21 +1,31 @@
 "use strict";
 const miBoton = document.getElementById('miBoton');
 
-// Agrega un manejador de clic al botón
-miBoton.addEventListener('click', function() {
-    // Verifica el contenido actual del botón
-    if (miBoton.textContent === '$15') {
-        // Si el contenido es "$15", cambia a "Jugar"
-        miBoton.textContent = 'Jugar';
-        miBoton.classList.add('clickeado');
-        // Después de un tiempo, quita la clase para volver al estado original
-        setTimeout(function() {
-        miBoton.classList.remove('clickeado');
-        }, 1000); // Cambiar el tiempo según tus preferencias
+let comprado= 'Comprado';
+let jugar='Jugar';
+let contenidoOriginal = miBoton.textContent;
 
-    } else {
-        // Si el contenido es diferente de "$15", cambia a "$15"
-        miBoton.textContent = '$15';
-    }
+// Agrega un manejador de clic al botón
+miBoton.addEventListener('click', function () {
+    // Verifica el contenido actual del botón
+    if (miBoton.textContent !== jugar) {
+        // Si el contenido es diferente de "Jugar", cambia a "Comprado"
+        miBoton.textContent = comprado;
+        miBoton.classList.add('clickeado');
+
+        // Después de un tiempo, quita la clase para volver al estado original
+        setTimeout(function () {
+            miBoton.classList.remove('clickeado');
+            // Después de otro tiempo, cambia el contenido a "Jugar"
+            setTimeout(function () {
+                miBoton.textContent = jugar;
+                // Después de 15 segundos, vuelve al contenido original
+                setTimeout(function () {
+                    miBoton.textContent = contenidoOriginal;
+                }, 15000); // 15 segundos en milisegundos
+            }, 1000); // 1 segundo en milisegundos
+        }, 1000); // 1 segundo en milisegundos
+    } 
 });
+
 
